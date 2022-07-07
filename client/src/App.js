2d1ac2464a4a0020870ee6007c63e0d4d01be69f
calvinjoshua <calvin@diamanteblockchain.com>
2022-07-07 16:29:12 +0530

    init commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
+import DS from "./contracts/Diamstore.json";
 import Diam from "./contracts/Diam.json";
-import getWeb3 from "./getWeb3";
 import "./App.css";
 import { ethers } from "ethers";
-// var ethUtil = require('ethereumjs-util');
-// var sigUtil = require('eth-sig-util');
-
 
 class App extends Component {
-  state = { allowance: 0, storageValue: "", web3: null, signer: null, accounts: null, contract: null };
+  state = { tokenC: null, storageValue: null, userToContract: null, balance: null, tokens: null, web3: null, signer: null, accounts: null, contract: null, chainId: null, network: null, allowance: 0 };
 
   componentDidMount = async () => {
     try {
@@ -22,9 +19,37 @@ class App extends Component {
 
       const accounts = await signer.getAddress()
 
-      const instance = new ethers.Contract("0x1e4ec7669CAD6CB8Cc5FDD7233Ef559112a015D2", Diam.abi, web3);
+      const instance = new ethers.Contract("0x8b861cE2e292B42e9f98345d3A17E14a4303A21E", Diam.abi, web3);
 
-      this.setState({ web3, signer, accounts, contract: instance });
+      const instance2 = new ethers.Contract("0xFE51787b5Eeb8467e36C430C33a2dE1399926A25", DS, web3);
+
+      const _chainId = await web3.getNetwork()
+
+      // console.log(_chainId)
+
+      this.setState({ chainId: _chainId.chainId })
+      this.setState({ network: _chainId.name })
+
+      const bal = await instance2.balanceOf(accounts)//await instance.balanceOf(accounts)
+
+      console.log("balance", bal.toString())
+
+      // console.log("balance ss", await instance2.balanceOf(accounts))
+
+      const _bal = ethers.utils.formatEther(bal.toString())
+
+      console.log("balance", _bal)
+
+      const response = await instance2.userTokens(accounts);
+      console.log("res ", response)
+      console.log("etherformat res", ethers.utils.formatEther(response.toString()));
+
+      this.setState({ storageValue: ethers.utils.formatEther(response.toString()) });
+
+
+
+
+      this.setState({ tokenC: instance, balance: _bal, web3, signer, accounts, contract: instance2 });
     } catch (error) {
       alert(
         `Failed to load web3, accounts, or contract. Check console for details.`,
@@ -36,28 +61,28 @@ class App extends Component {
 
   name = async () => {
     const { accounts, contract } = this.state;
-    // Get the value from the contract to prove it worked.
+
     const response = await contract.name();
     console.log("res ", response)
-    // Update state with the result.
+
     this.setState({ storageValue: response });
   };
 
   getAllowance = async () => {
     const { accounts, contract } = this.state;
     console.log(accounts)
-    // Get the value from the contract to prove it worked.
-    const response = await contract.allowance(accounts.toString(), "0xf796CBb51DcCdABfe3d7f7B3aA05db9eC89E8E28");
-    console.log("res ", response.toString())
-    // Update state with the result.
-    this.setState({ allowance: response.toString() });
+    const response = await contract.allowance(accounts.toString(), "0xFE51787b5Eeb8467e36C430C33a2dE1399926A25");
+    console.log("etherformat", ethers.utils.formatEther(response.toString()));
+    this.setState({ allowance: ethers.utils.formatEther(response.toString()) });
+    // console.log("res ", response.toString())
+    // this.setState({ allowance: response.toString() });
   };
 
   signData = async () => {
 
-    const { web3, signer, accounts, contract } = this.state;
+    const { web3, signer, accounts, contract, tokenC } = this.state;
 
-    console.log("signer", await signer.getAddress())
+    console.log("signer is", await signer.getAddress())
 
     var milsec_deadline = Date.now() / 1000 + 100;
 
@@ -67,7 +92,7 @@ class App extends Component {
 
     console.log(deadline, "sec");
 
-    var _nun = await contract.nonces(accounts.toString())
+    var _nun = await tokenC.nonces(accounts.toString())
 
     var nun = _nun.toString()
 
@@ -78,11 +103,11 @@ class App extends Component {
     console.log("netId", _chainId.chainId);
 
 
-    console.log("started")
+    console.log("started ", await signer.getAddress())
 
     const sig = await signer._signTypedData(
       {
-        name: "Diam", version: "1", chainId: _chainId, verifyingContract: "0x1e4ec7669CAD6CB8Cc5FDD7233Ef559112a015D2"
+        name: "Diam", version: "1", chainId: _chainId.chainId, verifyingContract: "0x8b861cE2e292B42e9f98345d3A17E14a4303A21E"
       },
       {
         Permit: [
@@ -109,9 +134,9 @@ class App extends Component {
         ],
       },
       {
-        owner: signer,
-        spender: "0xf796CBb51DcCdABfe3d7f7B3aA05db9eC89E8E28",
-        value: 10000000000,
+        owner: await signer.getAddress(),
+        spender: "0xFE51787b5Eeb8467e36C430C33a2dE1399926A25",
+        value: "20000000000000000000",
         nonce: nun,
         deadline: deadline
       }
@@ -122,7 +147,58 @@ class App extends Component {
     const { v, r, s } = ethers.utils.splitSignature(sig)
 
 
-    await contract.methods.permit(signer, "0xf796CBb51DcCdABfe3d7f7B3aA05db9eC89E8E28", 10000000000, deadline, v, r, s,).send({ from: accounts[0] });
+    console.log(v, " ", r, " ", s)
+
+    //contract.permit(await signer.getAddress(), "0xf796CBb51DcCdABfe3d7f7B3aA05db9eC89E8E28", "200000000000000000000", deadline, v, r, s,)
+
+
+    const ContractSigner = contract.connect(signer);
+
+    // Each DAI has 18 decimal places
+    // const dai = ethers.utils.parseUnits("1.0", 18);
+
+    // Send 1 DAI to "ricmoo.firefly.eth"
+    let tx = ContractSigner.depositWithPermit("20000000000000000000", deadline, v, r, s,);
+    console.log(await tx)
+
+
+
+  }
+
+
+  buyTokens = async (event) => {
+
+    event.preventDefault();
+    const { accounts, contract } = this.state;
+
+    let _web3 = new ethers.providers.JsonRpcBatchProvider("https://ropsten.infura.io/v3/0186bbd47475436b9e3ff3d644b4d21c")
+
+    let signer = new ethers.Wallet("238c0cd652acdd2145b3ee0a401c9f208b448d9ca27c0190cc0d93e4b5337764", _web3);
+
+    console.log("Sending tokens from " + (await signer.getAddress()).toString() + " to " + accounts.toString())
+
+    const contractWithSigner = contract.connect(signer);
+
+
+    let wei = ethers.utils.parseEther("100");
+
+    console.log("in wei", wei.toString())
+
+    let tx = await contractWithSigner.transfer(accounts, wei.toString());
+
+    console.log(tx.hash)
+
+    alert("Wait for transaction to confirm")
+
+  }
+
+
+  handleChange = async (event) => {
+    if (event.target.value > 101) {
+      alert("tokens exceed 100")
+      return
+    }
+    this.setState({ tokens: event.target.value });
   }
 
 
@@ -133,23 +209,41 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h2>EIP 712 Example</h2>
+        <div >
+          <h2>EIP 712 Example using ethers.js</h2>
+        </div>
+
 
         <div>connected wallet: {this.state.accounts}</div>
+        <br></br>
+        <div>connected wallet balance: {this.state.balance}</div>
+        <br></br>
+        <div>chainId : {this.state.chainId}  Network: {this.state.network}</div>
 
         <hr></hr>
 
-        <div>Allowance of {this.state.accounts} to 0xf796CBb51DcCdABfe3d7f7B3aA05db9eC89E8E28: {this.state.allowance}</div>
+        <h2>You can get 100 tokens by clicking below button</h2>
+
+        {/* <div>Allowance you have given to 0xFE51787b5Eeb8467e36C430C33a2dE1399926A25: {this.state.allowance}</div> */}
 
         <br></br>
 
-        <button onClick={() => this.name()}> get name </button>
+        <form onSubmit={this.buyTokens}>
+
+          <input type="submit" value="Get me some Tokens" />
+          {/* <p>you will get 100 tokens for testing it out !</p> */}
+        </form>
+
+        {/* <button onClick={() => this.buyTokens()}> get tokens </button> */}
+
         <br></br>
-        <button onClick={() => this.getAllowance()}> get allowance of owner to 0xf796CBb51DcCdABfe3d7f7B3aA05db9eC89E8E28</button>
-        <br></br>
-        <div>The contract name is: {this.state.storageValue}</div>
-        <br></br>
-        <button onClick={() => this.signData()}> Press to permit 0xf796CBb51DcCdABfe3d7f7B3aA05db9eC89E8E28  </button>
+
+        {/* <button onClick={() => this.getAllowance()}> Get allowance you gave to 0xFE51787b5Eeb8467e36C430C33a2dE1399926A25</button> */}
+<h2>Now Try to send 20 tokens to the smart contract (0xFE51787b5Eeb8467e36C430C33a2dE1399926A25) in a single transaction</h2>
+
+        <button onClick={() => this.signData()}> Send 20 tokens to the Smart contract  </button>
+
+        <p>Token sent to contract by the you so far {this.state.storageValue}</p>
       </div>
     );
   }
